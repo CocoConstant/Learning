57cf22abfccaff1fec5f25f94cb41c512ed54180
CocoCanstant <110228235+CocoCanstant@users.noreply.github.com>
2022-07-29 21:08:06 +0800

    Update create_file.sh
    add some describes to this file

--- create_file.sh
+++ create_file.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# the program is designed to create file of perl
+# the program is designed to create file of perl and you should give the running permission to this file.
 
 # obtain user file name
 file_name=$1.pl
@@ -7,16 +7,16 @@ file_name=$1.pl
 # create file
 $(touch "$file_name")
 
-# get running permission
+# get running permission to user
 $(chmod u+x "$file_name")
 
-# add basic character to file
+# add information of running environment to file
 echo "#!/usr/bin/perl" > $file_name
 
 # outputing information about creating file
-if [ -f $file_name ];
+if [ -f $file_name ];  # judging whether the file exist 
 then
-	if [ -e $file_name ];
+	if [ -e $file_name ]; # judging whether the content of file exist
 	then
 		echo the $file_name is created at $(date)
 	else
