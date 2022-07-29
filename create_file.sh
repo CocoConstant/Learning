4e5a00a2baaeeea7903ae395e9727b75cb2525e1
CocoCanstant <110228235+CocoCanstant@users.noreply.github.com>
2022-07-29 20:50:53 +0800

    Add files via upload
    this file is design to create Perl file

---
+++ create_file.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# the program is designed to create file of perl
+
+# obtain user file name
+file_name=$1.pl
+
+# create file
+$(touch "$file_name")
+
+# get running permission
+$(chmod u+x "$file_name")
+
+# add basic character to file
+echo "#!/usr/bin/perl" > $file_name
+
+# outputing information about creating file
+if [ -f $file_name ];
+then
+	if [ -e $file_name ];
+	then
+		echo the $file_name is created at $(date)
+	else
+		echo "can't add information to $file_name"
+	fi
+else
+	echo sorry the $file_name is not created
+fi
