2319fba7b1c5730f485c64ef39ff5bc5894f30e5
CocoConstant <kkang_ya_26@163.com>
2023-06-21 17:36:32 +0800

    up

--- daily_work/rename_Image.py
+++ daily_work/rename_Image.py
@@ -1,10 +1,40 @@
 import os
+import shutil
 import sys
 
 image_path = sys.argv[1]
-image = os.listdir(image_path)
+image_list = os.listdir(image_path)
 
-n = 0 # set the initial figure
-for i in image:
-    n += 1
-    os.rename(image_path+i, image_path + str(n) + '.' + 'tif')
+segment = 2 # how many pieces of photo
+x = 8
+y = 12
+initial_number = 1
+
+for _ in range(segment):
+    os.makedirs(f'/mnt/c/Users/ouyangkang/Desktop/Image_{_}' )
+
+
+dir_selection = 0 # select directory
+count = 0 # swith directory selection
+dir_0_count = 1
+dir_1_count = 0
+
+for image in image_list:
+    
+    shutil.copy(image_path + image, '/mnt/c/Users/ouyangkang/Desktop/Image_' + str(dir_selection) + '/' + str(dir_0_count if dir_selection == 0 else dir_1_count) + '.tif')
+    
+    judgement = initial_number % (x/segment)
+    if judgement == 0:
+        dir_selection = 1
+        count += 1
+    
+    if count == 2:
+        dir_selection = 0
+        count =0
+
+    if dir_selection == 0:
+        dir_0_count += 1
+    else:
+        dir_1_count += 1
+        
+    initial_number += 1
