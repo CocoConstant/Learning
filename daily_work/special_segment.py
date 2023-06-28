d625b3450bd5f2ee5fc9db5c92cfd24c66beb7e2
CocoConstant <kkang_ya_26@163.com>
2023-06-28 17:55:42 +0800

    up

--- daily_work/special_segment.py
+++ daily_work/special_segment.py
@@ -4,25 +4,21 @@ import shutil
 
 image_path = sys.argv[1]
 image_list = os.listdir(image_path)
-segment_dir = int(sys.argv[2])
 
+segment_dir = 2
 for _ in range(segment_dir):
     os.makedirs('/mnt/c/Users/ouyangkang/Desktop/Image_' + str(_))
 
-segment_num = sys.argv[3]
-segment_end = sys.argv[4]
+segment_start = int(sys.argv[2])
+segment_end = int(sys.argv[3])
 
 first = 0
 second = 0
-third = 0
 
 for num, image in enumerate(image_list):
-    if num + 1 <= segment_num:
+    if num % segment_end <= (segment_start - 1):
         first += 1
         shutil.copy(image_path + image, '/mnt/c/Users/ouyangkang/Desktop/Image_0' + '/' + str(first) + '.tif')
-    if num + 1 > segment_num and num + 1 <= segment_end:
+    else:
         second += 1
-        shutil.copy(image_path + image, '/mnt/c/Users/ouyangkang/Desktop/Image_1' + '/' + str(first) + '.tif')
-    if num + 1 > segment_end:
-        third += 1
-        judge = (num + 1) - (third*)
+        shutil.copy(image_path + image, '/mnt/c/Users/ouyangkang/Desktop/Image_1' + '/' + str(second) + '.tif')
